aabf98d24dc0565c559142ae8debe44f5ba98825
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-03-04 00:05:42 -0300

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var list = document.querySelectorAll(".question")
 
 list.forEach((question) => question.addEventListener('click', () => {
     if(question.classList.contains("invisible")) {
+        list.forEach((question) => question.classList.add("invisible"))
         question.classList.remove("invisible")
     } else {
         question.classList.add("invisible")
@@ -12,6 +13,7 @@ list.forEach((question) => question.addEventListener('click', () => {
     if(question.classList.contains("rotate")) {
         question.classList.remove("rotate")
     } else {
+        list.forEach((question) => question.classList.remove("rotate"))
         question.classList.add("rotate")
     }
 })) 
